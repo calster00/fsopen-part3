dc2b1d921930fc2f98219ccbb1d30b4e8bc8c8ea
calster00 <63422553+calster00@users.noreply.github.com>
2020-10-06 14:44:39 +0300

    part3 ex3.4

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ app.get('/api/persons/:id', (request, response) => {
   }
 });
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter(p => p.id != id);
+  response.status(204).end();
+})
+
 app.get('/info', (request, response) => {
   const time = new Date().toLocaleString();
   response.send(`

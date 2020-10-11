13a92548df6d9c89deb091b59163f9c066699121
calster00 <63422553+calster00@users.noreply.github.com>
2020-10-11 17:26:02 +0300

    part3 ex3.10-3.11

---
+++ build/precache-manifest.93a04910298beb7b5da839d4f9beb80e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffdc589fd0219dd41ce291b8c5eabc55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0d287ccc623384a462d",
+    "url": "/static/css/main.eb82856b.chunk.css"
+  },
+  {
+    "revision": "0f285c793074fdce220f",
+    "url": "/static/js/2.8454fdf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8454fdf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d287ccc623384a462d",
+    "url": "/static/js/main.1ea47fb4.chunk.js"
+  },
+  {
+    "revision": "c80949798ab70a174c70",
+    "url": "/static/js/runtime-main.bac6939f.js"
+  }
+]);

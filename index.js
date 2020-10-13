fb970121c262157d21b2c342dc0a6c9e550bd479
calster00 <63422553+calster00@users.noreply.github.com>
2020-10-13 19:38:44 +0300

    part3 ex3.13-3.14
    Signed-off-by: calster00 <63422553+calster00@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const cors = require('cors');
+const Person = require('./models/person');
 
 // morgan expects a format string, so we need to stringify object, otherwise
 // toString() will be called implicitly and we'll get "[object Object]"
@@ -14,65 +16,42 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :data')
 );
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-];
-
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook part3</h1>');
 });
 
-app.get('/info', (request, response) => {
-  const time = new Date().toLocaleString();
-  response.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${time}</p>
-  `);
-});
+// app.get('/info', (request, response) => {
+//   const time = new Date().toLocaleString();
+//   response.send(`
+//     <p>Phonebook has info for ${persons.length} people</p>
+//     <p>${time}</p>
+//   `);
+// });
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => {
+    response.json(persons);
+  })
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  const person = persons.find(p => p.id == id);
-
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  }).catch(e => {
+    response.status(400).json({
+      error: 'invalid id'
+    })
+  })
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter(p => p.id != id);
-  response.status(204).end();
-});
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = request.params.id;
+//   persons = persons.filter(p => p.id != id);
+//   response.status(204).end();
+// });
 
 app.post('/api/persons', (request, response) => {
   const body = request.body;
-  const newID = Math.floor(Math.random() * 1e6);
 
   if (!body.name) {
     return response.status(400).json({
@@ -86,23 +65,24 @@ app.post('/api/persons', (request, response) => {
     });
   }
 
-  if (persons.findIndex(p => p.name === body.name) != -1) {
-    return response.status(400).json({
-      error: 'contact already exists'
-    });
-  }
+  // if (persons.findIndex(p => p.name === body.name) != -1) {
+  //   return response.status(400).json({
+  //     error: 'contact already exists'
+  //   });
+  // }
 
-  const newPerson = {
+  const newPerson = new Person({
     name: body.name,
     number: body.number,
-    id: newID
-  };
+  });
   
-  persons = persons.concat(newPerson);
-  response.json(newPerson);
+  newPerson.save().then(savedPerson => {
+    console.log("added to the DB:", savedPerson);
+    response.json(savedPerson);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
